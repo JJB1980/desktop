c92f800a50e96f64062a8b9476176dbd0a404764
JJB1980 <john@jjbsw.com>
2015-03-27 13:26:26 +1000

    fix promise return for weather update

--- js/ngapp.js
+++ js/ngapp.js
@@ -93,12 +93,14 @@ app.controller('weatherController', function ($scope, $interval, mapService,
     
     $scope.updateWeather = function () {
         weatherService.update($scope.cities[$scope.currentCity]).then(function (data) {
-            $scope.weather = data.data.list[0];
-            $scope.weather.icon = $scope.icons[$scope.weather.weather[0].main] || '';
-            mapService.update($scope.weather.coord);
+            console.log('weather:');
+            console.log(data);       
+            if (data.list[0]) {
+                $scope.weather = data.list[0];
+                $scope.weather.icon = $scope.icons[$scope.weather.weather[0].main] || '';
+                mapService.update($scope.weather.coord);
+            }
             $scope.incrementCity();
-//            console.log('weather for: '+$scope.weather.name);
-//            console.log($scope.weather);       
         }, function (data) {
             LxNotificationService.error('Unable to load weather for '+$scope.cities[$scope.currentCity]);
             $scope.incrementCity();
@@ -156,7 +158,7 @@ app.factory('weatherService', function ($http, $q, LxProgressService) {
         update: function (city) {
             LxProgressService.linear.show('#5fa2db', '#weather-progress');
             var deferred = $q.defer();
-            return $http.get('http://api.openweathermap.org/data/2.5/find?q='+city+'&units=metric')
+            $http.get('http://api.openweathermap.org/data/2.5/find?q='+city+'&units=metric')
             .success(function (response) {
                 LxProgressService.linear.hide();
                 deferred.resolve(response);
@@ -165,8 +167,9 @@ app.factory('weatherService', function ($http, $q, LxProgressService) {
                 console.log('error:');
                 console.log(response);
                 LxProgressService.linear.hide();
-                deferred.reject(resposne);
+                deferred.reject(response);
             });
+            return deferred.promise;
         }
     };
 });
